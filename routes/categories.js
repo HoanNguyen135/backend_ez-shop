6de79eaff35ad4720cf858897ba5e1c7f413d7b8
unknown <hoannguyen13520@gmail.com>
2023-12-19 00:34:09 +0700

    push code

--- routes/categories.js
+++ routes/categories.js
@@ -2,6 +2,8 @@ const {Category} = require('../models/category');
 const express = require('express');
 const { User } = require('../models/user');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 router.get(`/`, async (req, res) =>{
     const categoryList = await Category.find();
@@ -32,6 +34,30 @@ router.post(`/deleteUser`, async (req, res) =>{
        return res.status(500).json({success: false, error: err}) 
     })
 })
+
+
+router.post('/addUser', async (req,res)=>{
+
+    let user = new User({
+        name: req.body?.name,
+        email: req.body?.email,
+        passwordHash: bcrypt.hashSync(req.body?.password, 10),
+        phone: req.body?.phone,
+        isAdmin: req.body?.isAdmin,
+        street: req.body?.street,
+        apartment: req.body?.apartment,
+        zip: req.body?.zip,
+        city: req.body?.city,
+        country: req.body?.country,
+    })
+    user = await user.save();
+
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
+
 
 router.post(`/updateuser`, async (req, res) =>{
 
